e086601739a80b195efec73303e7df049a573c7c
Vovchanchyk <oberigfast@gmail.com>
2021-09-30 15:20:24 +0300

    'demo'

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './stylesSrc/app.scss';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 
 
