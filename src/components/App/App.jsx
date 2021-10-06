e2edc7831d9d5617f0992c109e57c33e82529fe4
Vovchanchyk <oberigfast@gmail.com>
2021-10-06 19:18:25 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,12 @@ import { HashRouter as Router } from "react-router-dom";
 import { Main } from "../Main";
 import { Header } from "../Header";
 
+console.log('hallo')
 function App() {
   return (
     <Router>
       <div className="App">
+
         <Header />
         <Main />
       </div>
