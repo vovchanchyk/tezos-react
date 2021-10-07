f001384385fdc346fd95833fe2bacf66c5a9e73d
Vovchanchyk <oberigfast@gmail.com>
2021-10-07 13:04:31 +0300

    test

--- src/pages/Company/Company.jsx
+++ src/pages/Company/Company.jsx
@@ -16,7 +16,7 @@ const Company = () => {
    return (
       <Container img={data.img} className={className}>
          <Title className={styles.company__title}>{data.title}</Title >
-         <Description className={styles.company__description}>
+         <Description className={styles.company__description} >
             {data.description}
          </Description>
          <Socials className={styles.company__socials} />
